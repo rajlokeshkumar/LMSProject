7d32995b2cff87b93f245fd87f4276c3b043f99c
Lokeshkumar <rajlokeshkumar@gmail.com>
2018-02-07 10:58:07 +0530

    commit on files test

--- src/main/java/com/Logistic/jsfBean/HeyBuddy.java
+++ src/main/java/com/Logistic/jsfBean/HeyBuddy.java
@@ -3,14 +3,29 @@ package com.Logistic.jsfBean;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
 
 import com.LMS.jpaconnectivity.EmployeeJpadaoimpl;
 
 @ViewScoped
 @ManagedBean
-public class HeyBuddy {
+public class HeyBuddy implements PhaseListener {
 
 private String name;
+
+private String name1;
+
+
+
+public String getName1() {
+	return name1;
+}
+
+public void setName1(String name1) {
+	this.name1 = name1;
+}
 
 public HeyBuddy() {
 	
@@ -27,4 +42,22 @@ public void setName(String name) {
 public void performtheAction() {
 	System.out.println(this.getName());
 }
+@Override
+public void afterPhase(PhaseEvent event) {
+System.out.println(event.getPhaseId());
+	
+}
+@Override
+public void beforePhase(PhaseEvent event) {
+	System.out.println(event.getPhaseId());
+	
+}
+@Override
+public PhaseId getPhaseId() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+
 }
